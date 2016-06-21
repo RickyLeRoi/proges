5a046cb96ffb19536e5d41e1fe18908498ca955f
Riccardo <rickygiordy@hotmail.com>
2016-06-21 13:30:50 +0200

    risolto problema sessione quando manca il DB, migliorato il dump

--- DB/homeDB.php
+++ DB/homeDB.php
@@ -14,7 +14,9 @@
     <?php 	//header('Content-Type: text/plain');
 	session_start();
 	$user = $_SESSION['login_user'];
-	$userID = $_SESSION['id_user'];
+    if (!isset($_SESSION['login_user'])) {
+        session_destroy();
+    }
     ?>
 
     <style>
